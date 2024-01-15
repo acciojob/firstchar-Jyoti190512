f7160875f70ce5761d819d1d510427721f0598db
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2024-01-15 23:11:45 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstChar(text) {
   // your code here
+	for (let char of text) {
+        if (char !== ' ') {
+            return char;
+        }
+    }
+    // If the string only contains spaces or is empty
+    return '';
 }
 
 // Do not change the code below
